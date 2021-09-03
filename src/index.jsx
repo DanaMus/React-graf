f4d8f4fa915867090f650a011f41e730d08e533f
Dana Musilova <dana.musilova@email.cz>
2021-09-03 11:53:52 +0200

    Recharts - koláčový graf

--- src/index.jsx
+++ src/index.jsx
@@ -2,94 +2,115 @@ import React, { PureComponent } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie } from 'recharts';
 
+
+/* BarChart */
 const data = [
   {
     month: 'leden',
-    amount: 4000,
-    paid: 2400,
+    částka: 4000,
+    zaplaceno: 2400,
   },
   {
     month: 'únor',
-    amount: 5000,
-    paid: 2900,
+    částka: 5000,
+    zaplaceno: 2900,
   },
   {
     month: 'březen',
-    amount: 12500,
-    paid: 11100,
+    částka: 12500,
+    zaplaceno: 11100,
   },
   {
     month: 'duben',
-    amount: 8900,
-    paid: 8900,
+    částka: 8900,
+    zaplaceno: 12000,
   },
   {
     month: 'květen',
-    amount: 15000,
-    paid: 2100,
+    částka: 15000,
+    zaplaceno: 2100,
   },
   {
     month: 'červen',
-    amount: 11000,
-    paid: 11000,
+    částka: 11000,
+    zaplaceno: 11000,
   },
   {
     month: 'červenec',
-    amount: 4000,
-    paid: 2800,
+    částka: 4000,
+    zaplaceno: 2800,
   },
   {
     month: 'srpen',
-    amount: 14000,
-    paid: 12800,
+    částka: 14000,
+    zaplaceno: 12800,
   },
   {
     month: 'září',
-    amount: 8000,
-    paid: 800,
+    částka: 8000,
+    zaplaceno: 8800,
   },
   {
     month: 'říjen',
-    amount: 11000,
-    paid: 10500,
+    částka: 11000,
+    zaplaceno: 10500,
   },
   {
     month: 'listopad',
-    amount: 14800,
-    paid: 12500,
+    částka: 14800,
+    zaplaceno: 12500,
   },
   {
     month: 'prosinec',
-    amount: 22000,
-    paid: 22000,
+    částka: 22000,
+    zaplaceno: 22000,
   },
 ];
 
+/* PieChart */
+const data02 = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+  { name: 'Group E', value: 278 },
+  { name: 'Group F', value: 189 },
+];
 
 const App = () => {
   return (
-    <ResponsiveContainer width="100%" aspect={3}>
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="month" />
-        <YAxis />
-        <Tooltip />
-        {/* <Legend /> */}
-        <Bar dataKey="amount" stackId="a" fill="#8884d8" />
-        <Bar dataKey="paid" stackId="a" fill="#82ca9d" />
-      </BarChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          {/* <Legend /> */}
+          <Bar dataKey="částka" fill="#8884d8" />
+          <Bar dataKey="zaplaceno" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer width="100%" aspect={1}>
+        <PieChart width={400} height={400}>
+          <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
+      
+
+    </>
   )
 };
 
